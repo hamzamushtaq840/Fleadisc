5d39d5914b789b8d358c1e4aabb6bfe50ec2172b
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-03-26 16:40:27 +0500

    fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import signin from './../assets/signin.svg'
 import { NavLink, Outlet } from 'react-router-dom'
 
 const Navbar = () => {
 
+    const [showShadow,setShowShadow]=useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+    
+      const handleScroll = () => {
+        if(window.pageYOffset > 0){
+            setShowShadow(true);
+        } else if (window.pageYOffset === 0){
+            setShowShadow(false);
+        }
+      };
+
     return (
         <>
-            <div className='flex items-center fixed w-full h-[67px] justify-center bg-[#FAFAFA] z-50  '>
+            <div style={showShadow?{boxShadow:"0px 2px 12px rgba(0,0,0,0.1)"}:{}} className='flex items-center fixed w-full h-[67px] justify-center bg-[#FAFAFA] z-50'>
                 <div className='h-[67px] w-full  text-[1.2rem] sm:text-[0.75rem] xsm:text-[0.75rem]  font-sans font-[400] xsm:max-w-[100vw] sm:max-w-[100vw] max-w-[70vw] m-auto flex items-center justify-around gap-[1.256em] px-[1.125em]  '>
                     <NavLink to="/" className="nav-link flex flex-col gap-[3px] min-w-[50px] items-center  text-[#00000]" activeclassname="active" >
                         <svg width="19" height="19" ><path d="M0.125 0.125V8.45833H8.45833V0.125H0.125ZM6.375 6.375H2.20833V2.20833H6.375V6.375ZM0.125 10.5417V18.875H8.45833V10.5417H0.125ZM6.375 16.7917H2.20833V12.625H6.375V16.7917ZM10.5417 0.125V8.45833H18.875V0.125H10.5417ZM16.7917 6.375H12.625V2.20833H16.7917V6.375ZM10.5417 10.5417V18.875H18.875V10.5417H10.5417ZM16.7917 16.7917H12.625V12.625H16.7917V16.7917Z" /></svg>
