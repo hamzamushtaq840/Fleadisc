a926b1e95f891930807ca3a922d9158d6d21cb24
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2023-05-13 14:18:45 +0500

    finish 3

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,8 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <GoogleOAuthProvider clientId={`183852561589-lato7j05g878ral5tusueio0or5j6t8a.apps.googleusercontent.com`}>
         <div className='bg-[#FAFAFA] text-black'>
-          jad
-          {/* <App /> */}
+          <App />
         </div>
       </GoogleOAuthProvider>
     </AuthProvider>
